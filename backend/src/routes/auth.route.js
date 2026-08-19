1bf7c304b339bcab9ab0b5c2a163a337fb03d7f2
KazeemOluwanifemi <bethkaz934@gmail.com>
2026-08-19 15:52:48 +0100

    migration file included

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,4 +1,5 @@
-import express from "express"
-import { signupController } from "../controllers/auth.controller.js"
-export const authRoutes = express.Router()
-authRoutes.post("/signup", signupController)
+import express from "express";
+import { signupController } from "../controllers/auth.controller.js";
+
+export const authRoutes = express.Router();
+authRoutes.post("/signup", signupController);
--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,24 +1,41 @@
-import bcrypt from "bcrypt"
-import prisma from '../../lib/prisma.js';
-import { AppError } from './../utils/AppError.js';
-export const signupService = async ({ firstName, lastName, email, password }) => {
-    try {
-        const salt = 12
-        const passwordHash = await bcrypt.hash(password, salt)
-        const verificationToken = Math.floor(100000 + Math.random() * 900000)
-        const verificationTokenExpires = new Date(Date.now() + 10 * 60 * 1000)
-        const newUser = await prisma.user.create({
-            data: {
-                firstName, lastName, email, password: passwordHash, verificationToken, verificationTokenExpires
-            }
-        })
-        return newUser
-    } catch (error) {
-        if (error instanceof Prisma.PrismaClientKnownRequestError) {
-            if (error.code === "P2002") {
-                throw new AppError("cannot create an account with provided details", 409);
-            }
-        }
-        throw new AppError("internal server error", 500)
+import bcrypt from "bcrypt";
+import prisma from "../../lib/prisma.js";
+import { Prisma } from "../generated/prisma/client.ts";
+import { AppError } from "./../utils/AppError.js";
+export const signupService = async ({
+  firstName,
+  lastName,
+  email,
+  password,
+}) => {
+  try {
+    const salt = 12;
+    const passwordHash = await bcrypt.hash(password, salt);
+    const verificationToken = String(
+      Math.floor(100000 + Math.random() * 900000),
+    );
+    const verificationTokenExpiry = new Date(Date.now() + 10 * 60 * 1000);
+    const newUser = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password: passwordHash,
+        verificationToken,
+        verificationTokenExpiry,
+      },
+    });
+    return newUser;
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        console.log(error.meta);
+        throw new AppError(
+          "cannot create an account with provided details",
+          409,
+        );
+      }
     }
-}
+    throw new AppError("internal server error", 500);
+  }
+};
